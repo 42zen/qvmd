5a3b8633f8a7990de725c4da7306b54a2c38cc5f
zen <you@example.com>
2021-11-07 19:24:34 +0100

    Removed map structure allocation

--- src/map.c
+++ src/map.c
@@ -4,20 +4,14 @@ qvm_map_t   *map_new(void);
 
 qvm_map_t *map_new(void)
 {
-    qvm_map_t   *map;
-
-    // allocate the map entry
-    if (!(map = malloc(sizeof(*map)))) {
-        printf("Error: Couldn't allocate new map entry.\n");
-        return NULL;
-    }
+    static qvm_map_t    map;
 
     // set the default map entry values
-    map->section_id = -1;
-    map->address = 0;
-    *map->name = 0;
-    map->next = NULL;
+    map.section_id = -1;
+    map.address = 0;
+    *map.name = 0;
+    map.next = NULL;
 
     // return the new map entry
-    return map;
+    return &map;
 }
